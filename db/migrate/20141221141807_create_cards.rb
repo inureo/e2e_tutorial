d71b5ed59d0e39296731443cc791509b32a9c599
ryo.tezuka <tezuka0226@gmail.com>
2014-12-21 23:28:06 +0900

    Modify! migration of some models

--- db/migrate/20141221141807_create_cards.rb
+++ db/migrate/20141221141807_create_cards.rb
@@ -1,7 +1,9 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-
+      t.integer :user_id, null: false
+      t.integer :japanese_syllabary_id, null: false
+      t.string :text, null: false
       t.timestamps
     end
   end
--- db/migrate/20141221141820_create_japanese_syllabaries.rb
+++ db/migrate/20141221141820_create_japanese_syllabaries.rb
@@ -1,7 +1,7 @@
 class CreateJapaneseSyllabaries < ActiveRecord::Migration
   def change
     create_table :japanese_syllabaries do |t|
-
+      t.string :char, null: false
       t.timestamps
     end
   end
