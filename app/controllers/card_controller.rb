3ae955b1ab61f8fd63d60677491e246f42028a9c
ryo.tezuka <tezuka0226@gmail.com>
2014-12-22 10:24:21 +0900

    add card view

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,8 +1,10 @@
 class CardController < ApplicationController
   def view
+    @card = Card.find(params[:id])
   end
 
   def post
+    redirect_to login_path unless user_signed_in?
   end
 
   def delete
