3a615341dda96c996df247d0cdaf313bc71f9e2d
ryo.tezuka <tezuka0226@gmail.com>
2014-12-22 11:32:58 +0900

    Modify cards list page

--- app/controllers/japanese_syllabary_controller.rb
+++ app/controllers/japanese_syllabary_controller.rb
@@ -1,4 +1,7 @@
 class JapaneseSyllabaryController < ApplicationController
   def view
+    japanese_syllabary = JapaneseSyllabary.find(params['id'])
+    @char              = japanese_syllabary.char
+    @cards             = Card.where(japanese_syllabary_id: japanese_syllabary.id)
   end
 end
