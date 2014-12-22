45485db50b23125f7728a30fa9e23550102949c6
ryo.tezuka <tezuka0226@gmail.com>
2014-12-22 11:24:15 +0900

    Add post KARUTA flow

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -5,6 +5,20 @@ class CardController < ApplicationController
 
   def post
     redirect_to login_path unless user_signed_in?
+    save_post(params['card']) if params['card']
+
+    @chars = JapaneseSyllabary.all.to_a.map{ |char| [char.char, char.id] }
+  end
+
+  def save_post(params)
+    card = Card.create(
+      user_id: current_user.id,
+      japanese_syllabary_id: params['japanese_syllabary_id'],
+      text: params['text']
+    )
+
+    @card = card
+    render 'card/complete'
   end
 
   def delete
